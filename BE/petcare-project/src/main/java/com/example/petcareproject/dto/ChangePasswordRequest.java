5c1101e47c938a51315e0d6217def9fae2ba54ee
DianNg25 <nguyenncpc07985@fpt.edu.vn>
2024-11-26 19:53:12 +0700

    Change Password

---
+++ BE/petcare-project/src/main/java/com/example/petcareproject/dto/ChangePasswordRequest.java
@@ -0,0 +1,17 @@
+package com.example.petcareproject.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChangePasswordRequest {
+    private String userId;
+    private String currentPassword;
+    private String newPassword;
+
+}
